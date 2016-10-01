161f6397ade864df254f9f5e37ad1df97b052807
Kiddos <koubarlow@gmail.com>
2016-10-01 12:07:13 -0600

    Completed email auth

--- Pods/Target Support Files/Pods-Shine/Pods-Shine-frameworks.sh
+++ Pods/Target Support Files/Pods-Shine/Pods-Shine-frameworks.sh
@@ -84,8 +84,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKCoreKit/FBSDKCoreKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FBSDKLoginKit/FBSDKLoginKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
--- Shine/SignInVC.swift
+++ Shine/SignInVC.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailAddressTextField: MaterialTextField!
+    @IBOutlet weak var passwordTextField: MaterialTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func facebookLoginTapped(_ sender: AnyObject) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (user, error) in
+            if error != nil {
+                ProgressHUD.showError("Oops, \(error?.localizedDescription)")
+            } else if user?.isCancelled == true {
+                ProgressHUD.showSuccess("Facebook Login Cancelled")
+            } else {
+                ProgressHUD.showSuccess("Login into Facebook complete")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                ProgressHUD.showError("Oops, \(error?.localizedDescription)")
+            } else {
+                ProgressHUD.showSuccess("Connected to server")
+            }
+        })
+    }
+    
+    @IBAction func loginTapped(_ sender: AnyObject) {
+        
+        if let email = emailAddressTextField.text, let password = passwordTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    ProgressHUD.showSuccess("Let's go!")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            ProgressHUD.showError("Oops, \(error?.localizedDescription)")
+                        } else {
+                            ProgressHUD.showSuccess("Sign up successful!")
+                        }
+                    })
+                }
+            })
+        }
+    }
 }
