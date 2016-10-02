356c2f8c9085950b086330e67b9d26726c973763
Kiddos <koubarlow@gmail.com>
2016-10-02 09:30:26 -0600

    Finished Feed-UI

---
+++ Shine/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Shine
+//
+//  Created by Kiddos on 10/1/16.
+//  Copyright © 2016 Kiddos. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
